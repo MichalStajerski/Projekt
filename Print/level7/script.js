1532c5caa5f979c93fab6c38dcbd45d2c23757ed
Michal Stjaerski <m918560@gmail.com>
2021-07-21 15:33:05 +0200

    added localStorage also to this level

--- Print/level7/script.js
+++ Print/level7/script.js
@@ -1,6 +1,8 @@
-let lowerLimit  
-let upperLimit 
 const select = document.getElementById('mySelect')
+let lowerLimit   
+let upperLimit 
+let storageArray = []
+let option2 = document.createElement('option')
 
 function countBackWardsByOneIteration() {
     if(upperLimit>=lowerLimit){
@@ -22,8 +24,11 @@ function countBackWardsByOneIteration() {
             console.log(upperLimit)
         }
         select.add(option)
+        upperLimit--  
+        storageArray.push(option.text)
+        localStorage.setItem('options',storageArray.slice(-1).pop())
+        localStorage.setItem('upperLimit',upperLimit)
     }
-    upperLimit--  
 }
 
 function countForwardsByOneIteration(){ 
@@ -46,16 +51,33 @@ function countForwardsByOneIteration(){
             console.log(lowerLimit)
         }
         select.add(option)
+        lowerLimit++
+        storageArray.push(option.text)
+        localStorage.setItem('options',storageArray.slice(-1).pop())
+        localStorage.setItem('lowerLimit',lowerLimit)
+        console.log(localStorage)     
     }
-    lowerLimit++
 }
 function submitValues(){
     lowerLimit = parseInt(document.getElementById('lowerLimit').value)
     upperLimit = parseInt(document.getElementById('upperLimit').value)
+    localStorage.setItem('lowerLimit',lowerLimit)
+    localStorage.setItem('upperLimit',upperLimit)
 }
 
 function modulo(divident,divider){
     let scoreWithoutRestOfDivision = parseInt(divident / divider);  
     let dividentWithoutRestOfdivision = scoreWithoutRestOfDivision * divider;  
     return divident - dividentWithoutRestOfdivision;  
+}
+
+window.onload = function(){  
+    if(localStorage.getItem('options')!= null){
+        option2.text =  localStorage.getItem('options')
+        console.log('options',localStorage.getItem('options'))
+        lowerLimit = parseInt(localStorage.getItem('lowerLimit'))
+        upperLimit = parseInt(localStorage.getItem('upperLimit'))
+        console.log('onloadlowerlimit',lowerLimit)
+        select.add(option2)    
+    }
 }
