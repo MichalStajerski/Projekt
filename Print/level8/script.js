4541bf7fd163bba2ab3cdd692149ba326283f32f
Michal Stjaerski <m918560@gmail.com>
2021-07-24 21:03:58 +0200

    changed countBackwards function and added localStorage

--- Print/level8/script.js
+++ Print/level8/script.js
@@ -2,86 +2,90 @@
 let lowerLimit
 let upperLimit
 const select = document.getElementById('mySelect')
-const constraints = []
-let mod = []
+let constraints = []
+let moduloBool = []
 let counter = 1
 let counterarr = []
-let test = 1
+const storageArray = []
+const option2 = document.createElement('option')
+const parseEverySecond = (a1) => a1.every(x=> modulo(a1.indexOf(x),2)!=0,x = parseInt(x))
 
-const insteadOfLoop = (array) => array.map(el=>el)
 
-
-function countBackWardsByOneIteration () {
+function countBackWards () {
   if (upperLimit >= lowerLimit) {
     const option = document.createElement('option')
-    for(let i =1;i<constraints.length;i+=2){
-      mod.push(constraintForLowerLimitEqual(upperLimit,constraints[i]))
+    function countBackwards(i){
+      moduloBool.push(constraintForLowerLimitEqual(upperLimit,constraints[i]))
       if(constraintForLowerLimitEqual(upperLimit,constraints[i])==true){
         counter = i
         counterarr.push(counter)
       }
-      console.log(mod)
-      if(mod.includes(true) && countInArray(mod,true)>1){
+      console.log('mod',moduloBool)
+      if(moduloBool.includes(true) && countInArray(moduloBool,true)>1){
         for(let i =0;i<counterarr.length;i++){
           if(!option.text.includes(constraints[counterarr[i]-1]))
           option.text += constraints[counterarr[i]-1]
         }
       }
-      if(mod.includes(true) && countInArray(mod,true)==1){
+      if(moduloBool.includes(true) && countInArray(moduloBool,true)==1){
         option.text = constraints[counter-1]
       }
-      if(!mod.includes(true)){
+      if(!moduloBool.includes(true)){
         option.text = upperLimit
       }
+      if(i<constraints.length){
+        countBackwards(i+2)
+      }
     }
+    countBackwards(1)
+    
     counterarr = []
-    mod = []
+    moduloBool = []
     select.add(option)
     upperLimit--
-  }else{
-    setTimeout(() => {
-      alert('Wrong values')
-    }, 100)
+    storageArray.push(option.text)
+    localStorage.setItem('options', storageArray.slice(-1).pop())
+    localStorage.setItem('upperLimit', upperLimit)
   }
 }
 
-function countForwardsByOneIteration () {
+function countForwards () {
   if(lowerLimit <= upperLimit){
     const option = document.createElement('option') 
-    console.log('insteadofLoop',insteadOfLoop(constraints)) 
-    function action(i){
-      mod.push(constraintForLowerLimitEqual(lowerLimit,constraints[i]))
-      if(constraintForLowerLimitEqual(lowerLimit,constraints[i])==true){
+    
+    function actionCountForwad(i){
+      moduloBool.push(constraintForLowerLimitEqual(lowerLimit,constraints[i]))
+      if(constraintForLowerLimitEqual(lowerLimit,constraints[i])){
         counter = i
         counterarr.push(counter)
+        console.log('counterarr',counterarr)
       }
-      console.log(mod)
-      if(mod.includes(true) && countInArray(mod,true)>1){
+      console.log(moduloBool)
+      if(moduloBool.includes(true) && countInArray(moduloBool,true)>1){
         for(let i =0;i<counterarr.length;i++){
           if(!option.text.includes(constraints[counterarr[i]-1]))
           option.text += constraints[counterarr[i]-1]
         }
       }
-      if(mod.includes(true) && countInArray(mod,true)==1){
+      if(moduloBool.includes(true) && countInArray(moduloBool,true)==1){
         option.text = constraints[counter-1]
       }
-      if(!mod.includes(true)){
+      if(!moduloBool.includes(true)){
         option.text = lowerLimit
       }
       if(i<constraints.length){
-        action(i+2)
+        actionCountForwad(i+2)
       }
     }
-    action(test)
+    actionCountForwad(1)
 
     counterarr = []
-    mod = []
+    moduloBool = []
     select.add(option)
     lowerLimit++
-  }else{
-    setTimeout(() => {
-      alert('Wrong values')
-    }, 100)
+    storageArray.push(option.text)
+    localStorage.setItem('options', storageArray.slice(-1).pop())
+    localStorage.setItem('lowerLimit', lowerLimit)
   }
 }  
 
@@ -94,12 +98,13 @@ function countForwardsByOneIteration () {
 function submitValues () {
   lowerLimit = parseInt(document.getElementById('lowerLimit').value)
   upperLimit = parseInt(document.getElementById('upperLimit').value)
-  console.log('loweLimit',lowerLimit)
-  console.log('uppperLimit',upperLimit)
+  localStorage.setItem('lowerLimit', lowerLimit)
+  localStorage.setItem('upperLimit', upperLimit)
 }
 function submitWordsAndDivisors () {
   constraints.push(document.getElementById('word').value)
   constraints.push(parseInt(document.getElementById('divisor').value))
+  localStorage.setItem('constraints',constraints )
   console.log(constraints)  
 }
 
@@ -113,25 +118,25 @@ function countInArray(array, what) {
   return array.filter(item => item == what).length;
 }
 
-//instead of going by loop through each element we can use map and defined function f that will make changes to each item for example
-//will use recursive version to swap all the loops
-// const arr = [2,3]
-// const iterate = (a1) => a1.map(res => res )
-// console.log(iterate(arr))
+window.onload = function () {
+  if (localStorage.getItem('options') != null) {
+    option2.text = localStorage.getItem('options')
+    console.log('options', localStorage.getItem('options'))
+    lowerLimit = parseInt(localStorage.getItem('lowerLimit'))
+    upperLimit = parseInt(localStorage.getItem('upperLimit'))
+    constraints = localStorage.getItem('constraints').split(',')
 
-// console.log(map(f,arr,4))
-// function map(f, a,divider) {
-//   if (a.length === 0) { return []; }
-//   return [f(a[0],divider)].concat(map(f, a.slice(1),divider));
-// }
-// function f(item){
-//   if(!option.text.includes(constraints[counterarr[item]-1]))
-//   return option.text += constraints[counterarr[item]-1]
-// }
-// function f(item,divider){
-//   if(constraintForLowerLimitEqual(item,divider)==true){
-//     return item+' => jest podzielne przez '+divider
-//   }else{
-//     return item+' => nie jest podzielne przez '+divider
-//   }
-// }
+    // function parseEverySecondElementToInt(i){
+    //   constraints[i] = parseInt(constraints[i])
+    //   if(constraints.length>i){
+    //     parseEverySecondElementToInt(i+2)
+    //   }
+    // }
+    // parseEverySecond(constraints)
+    for(let i =1;i<constraints.length;i+=2){
+      constraints[i] = parseInt(constraints[i])
+    }
+    console.log(constraints)
+    select.add(option2)
+  }
+}
