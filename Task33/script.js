3c850b29d39e9431a8991ee7255a861a578f8f01
Michal Stjaerski <m918560@gmail.com>
2021-07-26 15:28:58 +0200

    added submitting answers and different available paces of ball

--- Task33/script.js
+++ Task33/script.js
@@ -2,9 +2,10 @@ var canvas = document.getElementById( "myCanvas" );
 var context = canvas.getContext( "2d" );
 var width = 900;
 var height = 600;
-let counter = 0
-let lastColor = 0
+let counterOfHits = 0
+let lastColor
 let timeCounter = 0
+
 
 // const colors = {
 //     color: 'blue',
@@ -23,8 +24,8 @@ var ball = {
     x: 100,
     y: 100,
     radius: 25,
-    xSpeed: 2,
-    ySpeed: 2,
+    xSpeed: 0,
+    ySpeed: 0,
 }
 function draw( ctx,color,x,y,radius ) {
     ctx.beginPath();
@@ -36,63 +37,88 @@ function move(ball) {
     ball.x += ball.xSpeed;
     ball.y += ball.ySpeed;
 }
+function start(chosenSpeed){
+    ball.xSpeed =chosenSpeed
+    ball.ySpeed =chosenSpeed
 
-var timer = setInterval(() => {
-    let time = document.getElementById('timer')
-    time.innerHTML= timeCounter
-    timeCounter+=1
-}, 1000);
+    var timer = setInterval(() => {
+        let time = document.getElementById('timer')
+        time.innerHTML = 'Timer: '+ timeCounter
+        timeCounter++
+        
+    }, 1000);
+      
+    var action = setInterval(() => {
+        doAction()
+    }, 10);
+    
+    function doAction(){
+        context.clearRect( 0, 0, width, height );
+        context.strokeRect( 0, 0, width, height );
+        
+        move(ball);
+        
+        // right 
+        if ( ball.x + ball.radius >= width ) {
+            ball.x = width - ball.radius;
+            ball.xSpeed = -ball.xSpeed;
+            lastColor = Math.floor(Math.random()*colors.length)
+            draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
+            counterOfHits++
+        }
+        
+        // left
+        if ( ball.x - ball.radius <= 0 ) {
+            ball.x = ball.radius;
+            ball.xSpeed = -ball.xSpeed;
+            lastColor = Math.floor(Math.random()*colors.length)
+            draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
+            counterOfHits++
+        }
+        
+        // down
+        if ( ball.y + ball.radius >= height ) {
+            ball.y = height - ball.radius;
+            ball.ySpeed = -ball.ySpeed;
+            lastColor = Math.floor(Math.random()*colors.length)
+            draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
+            counterOfHits++
+        }
+        
+        // up
+        if ( ball.y - ball.radius <= 0 ) {
+            ball.y = ball.radius;
+            ball.ySpeed = -ball.ySpeed;
+            lastColor = Math.floor(Math.random()*colors.length)
+            draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
+            counterOfHits++
+        }
+        console.log('counterOfHits=> ',counterOfHits)
+        draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
+        if(timeCounter>10){
+            clearInterval(action)
+            clearInterval(timer)
+        }
+    }
+}
 
+function submitAnswer(){
+    const asnwer = parseInt(document.getElementById('submittedAnswer').value)
+    if(asnwer===counterOfHits){
+        alert('Victory')
+    }else alert('Wrong Answer')
+}
 
-var action = setInterval(() => {
-    doAction()
-}, 10);
-
-function doAction(){
-    context.clearRect( 0, 0, width, height );
-    context.strokeRect( 0, 0, width, height );
-    
-    move(ball);
-    
-    // right 
-    if ( ball.x + ball.radius >= width ) {
-    	ball.x = width - ball.radius;
-        ball.xSpeed = -ball.xSpeed;
-        lastColor = Math.floor(Math.random()*colors.length)
-        draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
-        counter++
-    }
-    
-    // left
-    if ( ball.x - ball.radius <= 0 ) {
-    	ball.x = ball.radius;
-        ball.xSpeed = -ball.xSpeed;
-        lastColor = Math.floor(Math.random()*colors.length)
-        draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
-        counter++
-    }
-    
-    // down
-    if ( ball.y + ball.radius >= height ) {
-    	ball.y = height - ball.radius;
-        ball.ySpeed = -ball.ySpeed;
-        lastColor = Math.floor(Math.random()*colors.length)
-        draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
-        counter++
-    }
-    
-    // up
-    if ( ball.y - ball.radius <= 0 ) {
-    	ball.y = ball.radius;
-        ball.ySpeed = -ball.ySpeed;
-        lastColor = Math.floor(Math.random()*colors.length)
-        draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
-        counter++
-    }
-    console.log('counter=> ',counter)
-    draw( context,colors[lastColor],ball.x,ball.y,ball.radius);
-    if(timeCounter>=5){
-        clearInterval(action)
-        clearInterval(timer)
+function setPace(id){
+    switch(id){
+        case 'Slow':
+            start(3)
+        break;
+        case 'Medium':
+            start(8)
+        break;
+        case 'Fast':
+            start(13)
+        break;
     }
 }
