931f275d8fb7c7897ecadc8215aff77a978bf6cf
Michal Stajerski <m918560@gmail.com>
2021-08-06 11:38:12 +0200

    added an answer and randomly drawing an order of words from it

--- Task30/script.js
+++ Task30/script.js
@@ -1,10 +1,13 @@
 let dragindex=0
 let dropindex=0
 let clone=""
+const answer = 'Ala ma kota czarnego kota'
+const words = answer.split(' ')
+let order = []
+console.log('words',words)
 
-function drag(e)
-{
-e.dataTransfer.setData("text",e.target.id)
+function drag(e){
+    e.dataTransfer.setData("text",e.target.id)
 }
 
 function drop(e){
@@ -17,7 +20,6 @@ function drop(e){
             dragindex=i
         }
     }
-
     document.getElementById("parent").replaceChild(document.getElementById(data),e.target)
     document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex])
 }
@@ -25,3 +27,22 @@ function drop(e){
 function allowDrop(e){
     e.preventDefault()
 }
+
+function drawOrderOfWords(){
+    for(let i = 0;i<words.length;i++){
+        order.push(i)
+    }
+    console.log(order)
+    shuffleArray(order)
+    console.log(order)
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+drawOrderOfWords()
