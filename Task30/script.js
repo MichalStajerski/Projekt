6f7140ab32cc16817336f8e6e791b03ead3ff631
Michal Stajerski <m918560@gmail.com>
2021-08-06 12:28:27 +0200

    added creating front dynamically based on number of words

--- Task30/script.js
+++ Task30/script.js
@@ -1,27 +1,27 @@
 let dragindex=0
 let dropindex=0
 let clone=""
-const answer = 'Ala ma kota czarnego kota'
+const answer = 'Ala ma czarnego kota'
 const words = answer.split(' ')
 let order = []
 console.log('words',words)
 
 function drag(e){
-    e.dataTransfer.setData("text",e.target.id)
+    e.dataTransfer.setData('text',e.target.id)
 }
 
 function drop(e){
     e.preventDefault()
     clone=e.target.cloneNode(true)
-    let data=e.dataTransfer.getData("text");
-    let nodelist=document.getElementById("parent").childNodes
+    let data=e.dataTransfer.getData('text');
+    let nodelist=document.getElementById('parent').childNodes
     for(let i=0;i<nodelist.length;i++){
         if(nodelist[i].id==data){
             dragindex=i
         }
     }
-    document.getElementById("parent").replaceChild(document.getElementById(data),e.target)
-    document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex])
+    document.getElementById('parent').replaceChild(document.getElementById(data),e.target)
+    document.getElementById('parent').insertBefore(clone,document.getElementById('parent').childNodes[dragindex])
 }
 
 function allowDrop(e){
@@ -37,6 +37,12 @@ function drawOrderOfWords(){
     console.log(order)
 }
 
+function insertWordIntoDiv(array){
+    for(let i = 0; i < array.length;i++){
+        document.getElementById('word'+i).innerHTML = words[array[i]]
+    }
+}
+
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -45,4 +51,23 @@ function shuffleArray(array) {
         array[j] = temp;
     }
 }
-drawOrderOfWords()
+
+function createLayout(array,event){
+    // const numWords = array.length
+    const container = document.getElementById('parent')
+    for(let i = 0;i<array.length;i++){
+        const word = document.createElement('div')
+        word.id='word'+i
+        word.className = 'droptarget'
+        word.draggable= 'true' 
+        word.setAttribute("ondragstart","drag(event)")
+        word.setAttribute("ondrop","drop(event)")
+        word.setAttribute("ondrAGOVER","allowDrop(event)") 
+        container.appendChild(word)
+    }
+}
+window.onload = function(){
+    createLayout(words)
+    drawOrderOfWords()
+    insertWordIntoDiv(order)
+}
