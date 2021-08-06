bb459b6edcad5024f86eb2da54e009f124d6da5a
Michal Stajerski <m918560@gmail.com>
2021-08-06 09:58:12 +0200

    code cleanup

--- Task30/script.js
+++ Task30/script.js
@@ -7,28 +7,22 @@ function drag(e)
 e.dataTransfer.setData("text",e.target.id);
 }
 
-function drop(e)
-{
-e.preventDefault();
-clone=e.target.cloneNode(true);
-let data=e.dataTransfer.getData("text"); 
-let nodelist=document.getElementById("parent").childNodes;
-for(let i=0;i<nodelist.length;i++)
-{
-if(nodelist[i].id==data)
-{
-dragindex=i;
+function drop(e){
+    e.preventDefault();
+    clone=e.target.cloneNode(true);
+    let data=e.dataTransfer.getData("text"); 
+    let nodelist=document.getElementById("parent").childNodes;
+    for(let i=0;i<nodelist.length;i++){
+        if(nodelist[i].id==data){
+            dragindex=i;
+        }
+    }
+
+    document.getElementById("parent").replaceChild(document.getElementById(data),e.target);
+
+    document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex]);
 }
 
-}
-
-document.getElementById("parent").replaceChild(document.getElementById(data),e.target);
-
-document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex]);
-
-}
-
-function allowDrop(e)
-{
+function allowDrop(e){
     e.preventDefault();
 }
