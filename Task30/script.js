ed3f9acb1215c56965d798d9559d09c8dbd34761
Michal Stajerski <m918560@gmail.com>
2021-08-06 09:54:56 +0200

    added swapping of words on drag and drop

--- Task30/script.js
+++ Task30/script.js
@@ -1,14 +1,34 @@
-function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id);
-  }
-  
-  function allowDrop(event) {
-    event.preventDefault();
-  }
-  
-  function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("Text");
-    console.log('eventTargetID',event.target.id)
-    event.target.append(document.getElementById(data));
-  }
+let dragindex=0;
+let dropindex=0;
+let clone="";
+
+function drag(e)
+{
+e.dataTransfer.setData("text",e.target.id);
+}
+
+function drop(e)
+{
+e.preventDefault();
+clone=e.target.cloneNode(true);
+let data=e.dataTransfer.getData("text"); 
+let nodelist=document.getElementById("parent").childNodes;
+for(let i=0;i<nodelist.length;i++)
+{
+if(nodelist[i].id==data)
+{
+dragindex=i;
+}
+
+}
+
+document.getElementById("parent").replaceChild(document.getElementById(data),e.target);
+
+document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex]);
+
+}
+
+function allowDrop(e)
+{
+    e.preventDefault();
+}
