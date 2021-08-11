e43fc37fa56205952bff4d963ffca6ff40b77684
Michal Stajerski <m918560@gmail.com>
2021-08-11 09:37:34 +0200

    added droping puzzle piece and checking answer

--- Task4/script.js
+++ Task4/script.js
@@ -61,7 +61,11 @@ function buildPieces () {
       yPos += pieceHeight
     }
   }
-  document.onmousedown = shufflePuzzle
+  //show the user image to remember for 2 seconds then present the puzzle 
+  setTimeout(()=>{
+   shufflePuzzle()
+  },2000)
+ 
 }
 function shufflePuzzle () {
   shuffleArray(pieces)
@@ -95,11 +99,11 @@ function onPuzzleClick (e) {
   if (e.layerX || e.layerY == 0) { // e.layerX returns the current coordiante of the place where event happened, our mouse position
     mouse.x = e.layerX - canvas.offsetLeft // offsetLeft read-only property returns the number of pixels that the upper left corner of the current element is offset to the left within the HTMLElement.offsetParent node
     mouse.y = e.layerY - canvas.offsetTop
-  } else if (e.offsetX || e.offsetX == 0) { // offsetX read-only property of the MouseEvent interface provides the offset in the X coordinate of the mouse pointer between that event and the padding edge of the target node
+  } else if (e.offsetX || e.offsetY == 0) { // offsetX read-only property of the MouseEvent interface provides the offset in the X coordinate of the mouse pointer between that event and the padding edge of the target node
     mouse.x = e.offsetX - canvas.offsetLeft
     mouse.y = e.layerY - canvas.offsetTop
   }
-  mouse.y += 100 // added it cause without it often not the tiles that were clicked on got dragged but different one
+  mouse.y += 89 // added it cause without it often not the tiles that were clicked on got dragged but different one
   console.log('mouse.x,mouse.y', mouse.x, mouse.y)
   currentPiece = checkPieceClicked()
   if (currentPiece != null) {
@@ -108,6 +112,7 @@ function onPuzzleClick (e) {
     context.drawImage(img, currentPiece.sx, currentPiece.sy, pieceWidth, pieceHeight, mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight)
     context.restore()
     document.onmousemove = updatePuzzle
+    document.onmouseup = pieceDropped
   }
 }
 
@@ -156,26 +161,41 @@ function updatePuzzle (e) {
     context.restore()
     context.strokeRect(mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight)
 }
-//TODO finish function for dropping puzzle piece then write algorithm for checking if the puzzle is completed
-//center canvas - but not through css since the clicking on puzzle pices wont move  
-//for now the puzzle is being made after clicks, need to change so that user can see photo for a set period of time
-//then photo disapears and he gets puzzle pieces that need to be reshuffled so we receive the original image
-//might as well add a set of pictures and a method to draw them from images folder
-function dropPuzzlePice(){
 
+function pieceDropped(e){
+  document.onmousemove = null;
+  document.onmouseup = null;
+  if(currentDropPiece != null){
+      var tmp = {xPos:currentPiece.xPos,yPos:currentPiece.yPos};
+      currentPiece.xPos = currentDropPiece.xPos;
+      currentPiece.yPos = currentDropPiece.yPos;
+      currentDropPiece.xPos = tmp.xPos;
+      currentDropPiece.yPos = tmp.yPos;
+  }
+  checkAnswer();
 }
-function checkAnwer(){
 
+function checkAnswer(){
+  context.clearRect(0,0,puzzleWidth,puzzleHeight);
+  var gameWin = true;
+  var piece;
+  for(let i = 0;i < pieces.length;i++){
+      piece = pieces[i];
+      context.drawImage(img, piece.sx, piece.sy, pieceWidth, pieceHeight, piece.xPos, piece.yPos, pieceWidth, pieceHeight);
+      context.strokeRect(piece.xPos, piece.yPos, pieceWidth,pieceHeight);
+      if(piece.xPos != piece.sx || piece.yPos != piece.sy){
+          gameWin = false;
+      }
+  }
+  if(gameWin){
+      document.onmousedown = null,
+      document.onmousemove = null,
+      document.onmouseup = null,
+      setTimeout(()=>   
+      alert('victory'),500)
+  }
 }
-let a = 3
-let b = 7
-a = a ^ b
-console.log('a1',a)
-b = a ^ b
-console.log('b',b)
-a = a ^ b
-console.log('a2',a)
-  
+
 window.onload = function () {
   img = new Image()
   img.addEventListener('load', onImage, false)
