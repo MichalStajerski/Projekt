f512c63cfb3b1644511f1cd225ffaf89ec1f5e63
Michal Stajerski <m918560@gmail.com>
2021-08-12 09:28:18 +0200

    front changes and small fixes

--- Task34/script.js
+++ Task34/script.js
@@ -1,5 +1,5 @@
-const numLeters = 4
-const numNumbers = 4
+const numLeters = 3
+const numNumbers = 3
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 const colors = {
   default: 'lightgrey',
@@ -26,19 +26,30 @@ function createLayout () {
       } else {
         square.id = 'square' + rowIndex
       }
+      console.log('squareid',square.id)
       square.className = 'square'
       square.style.backgroundColor = colors.default
       col.appendChild(square)
     }
     layout.appendChild(col)
   }
+  const asnwerBoard = document.getElementById('answerBoard')
+
+  for(let i = 0; i < answers.length; i++){
+    const answer = document.createElement('div')
+    answer.id = 'answer'
+    answer.className ='answer'
+    answer.innerHTML = answers[i]
+    asnwerBoard.appendChild(answer)
+  }
+  
   fillSignsForLayout()
   console.log('next', document.getElementById('col1').children[0])
 }
 function fillSignsForLayout () {
   for (let i = 1; i < numNumbers + 1; i++) {
     document.getElementById('col0').children[i].innerHTML = i// first column except first and last
-    document.getElementById('col' + (numNumbers + 1)).children[i].innerHTML = i// last column except first and last
+    document.getElementById('col' + (numLeters + 1)).children[i].innerHTML = i// last column except first and last
   }
   for (let i = 1; i < numLeters + 1; i++) {
     document.getElementById('col' + i).children[0].innerHTML = alphabet[i - 1]// first row except first and last element
@@ -87,6 +98,10 @@ function squareClicked (square) {
 }
 
 function drawAnswers () {
+  //first we need to decide number of coordinates to find
+  //that number is related to the difficulty lvl so i assume its a fixed number accrding to each lvl 
+  //so i wont imlpement here any algorithm to deduce that number based on the surface of the board we are given
+
 
 }
 
