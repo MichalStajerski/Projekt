66dd740a0366dc4f5d92246c28c6117627b77a22
Michal Stajerski <m918560@gmail.com>
2021-08-17 09:30:01 +0200

    drawing new pipes after click fixed

--- Task36/script.js
+++ Task36/script.js
@@ -9,7 +9,6 @@ let puzzleHeight
 let pieceWidth
 let pieceHeight
 let currentPiece // current piece that we are draggin
-let currentDropPiece // piece that the currently dgraged piece will be droped upon
 let mouse // will store coordiantes of the cursor when clicked it performs action
 let clickCounter = 0
 
@@ -67,36 +66,35 @@ function buildPieces () {
   }, 2000)
 }
 function shufflePuzzle () {
-  // shuffleArray(pieces)
   context.clearRect(0, 0, puzzleWidth, puzzleHeight) // sets the pixels in rect to transparent black
   let xPos = 0
   let yPos = 0
+  let angle = 0
+  let pieceClicked = 0
   for (let i = 0; i < pieces.length; i++) {
     const piece = pieces[i] // takes every puzzle piece from puzzle
     piece.xPos = xPos
     piece.yPos = yPos
-    context.translate(piece.xPos + pieceWidth, piece.yPos)
-    context.rotate(90 * Math.PI / 180)
-    context.drawImage(img, xPos, yPos, pieceWidth, pieceHeight, -xPos / 1000, -yPos / 1000, pieceWidth, pieceWidth)
-    context.rotate(-(90 * Math.PI / 180))
-    context.translate(-(piece.xPos + pieceWidth), -(piece.yPos))
+    piece.angle = angle
+    piece.pieceClicked = pieceClicked
+
+    context.drawImage(img, piece.sx, piece.sy, pieceWidth, pieceHeight, xPos, yPos, pieceWidth, pieceHeight)
+    context.strokeRect(xPos, yPos, pieceWidth, pieceHeight)
+
+    // context.translate(piece.xPos + pieceWidth, piece.yPos)
+    // context.rotate(90 * Math.PI / 180)
+    // context.drawImage(img, xPos, yPos, pieceWidth, pieceHeight, -(pieceWidth/100), -(pieceHeight/1000), pieceWidth, pieceWidth)
+    // context.rotate(-(90 * Math.PI / 180))
+    // context.translate(-(piece.xPos + pieceWidth), -(piece.yPos))
+    // angle = 90 
     xPos += pieceWidth
     if (xPos >= puzzleWidth) { // sets to new row when we reach the end of an x axis of our canvas
       xPos = 0
       yPos += pieceHeight
     }
   }
-  document.onmousedown = onPuzzleClick
 }
-function shuffleArray (array) { // randomly shuffles our array
-  for (let i = 0; i < array.length; i++) {
-    const j = Math.floor(Math.random() * (i + 1))
-    const temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-}
-
+document.onmousedown = onPuzzleClick
 function onPuzzleClick (e) {
   clickCounter++
   console.log('ckickCounter', clickCounter)
@@ -108,16 +106,21 @@ function onPuzzleClick (e) {
     mouse.x = e.offsetX - canvas.offsetLeft
     mouse.y = e.layerY - canvas.offsetTop
   }
-  mouse.y += 89 // added it cause without it often not the tiles that were clicked on got dragged but different one
+  
   console.log('mouse.x,mouse.y', mouse.x, mouse.y)
   currentPiece = checkPieceClicked()
   console.log('currentPiece', currentPiece)
   if (currentPiece != null) {
-    context.translate(currentPiece.xPos + pieceWidth, currentPiece.yPos + pieceHeight)
-    context.rotate(180 * Math.PI / 180)
-    context.drawImage(img, currentPiece.xPos, currentPiece.yPos, pieceWidth, pieceHeight, -currentPiece.xPos / 1000, -currentPiece.yPos / 1000, pieceWidth, pieceWidth)
-    context.rotate(-(180 * Math.PI / 180))
-    context.translate(-(currentPiece.xPos + pieceWidth), -(currentPiece.yPos + pieceHeight))
+    context.save()
+    context.translate(currentPiece.xPos+62, currentPiece.yPos+62)
+    context.rotate((currentPiece.angle+90) * Math.PI / 180)
+    context.drawImage(img, currentPiece.xPos, currentPiece.yPos, pieceWidth, pieceHeight, -(pieceWidth/2), -(pieceHeight/2), pieceWidth, pieceWidth)
+    context.translate(-(currentPiece.xPos), -(currentPiece.yPos))
+    context.restore()
+    currentPiece.angle +=90 
+    if(currentPiece.angle>360){
+      currentPiece.angle = 0
+    }
   }
 }
 
@@ -132,74 +135,6 @@ function checkPieceClicked () {
     }
   }
   return null // in case we dont get any piece
-}
-
-function updatePuzzle (e) {
-  currentDropPiece = null
-  if (e.layerX || e.layerY == 0) {
-    mouse.x = e.layerX - canvas.offsetLeft
-    mouse.y = e.layerY - canvas.offsetTop
-  } else if (e.offsetX || e.offsetY == 0) {
-    mouse.x = e.offsetX - canvas.offsetLeft
-    mouse.y = e.offsetY - canvas.offsetTop
-  }
-  context.clearRect(0, 0, puzzleWidth, puzzleHeight)
-  for (i = 0; i < pieces.length; i++) {
-    const piece = pieces[i]
-    if (piece == currentPiece) {
-      continue
-    }
-    context.drawImage(img, piece.sx, piece.sy, pieceWidth, pieceHeight, piece.xPos, piece.yPos, pieceWidth, pieceHeight)
-    context.strokeRect(piece.xPos, piece.yPos, pieceWidth, pieceHeight)
-    if (currentDropPiece == null) {
-      if (mouse.x > piece.xPos && mouse.x < (piece.xPos + pieceWidth) && mouse.y > piece.yPos && mouse.y < (piece.yPos + pieceHeight)) {
-        currentDropPiece = piece
-        context.save()
-        context.globalAlpha = 0.3 // makes the puzzle piece that we are about to drop our currently dragged piece upon less transparent
-        context.fillRect(currentDropPiece.xPos, currentDropPiece.yPos, pieceWidth, pieceHeight)
-        context.restore()// we are done with using alpha property and restore them to what they were
-      }
-    }
-  }
-  // redraw the dragged piece
-  context.save()
-  context.drawImage(img, currentPiece.sx, currentPiece.sy, pieceWidth, pieceHeight, mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight)
-  context.restore()
-  context.strokeRect(mouse.x - (pieceWidth / 2), mouse.y - (pieceHeight / 2), pieceWidth, pieceHeight)
-}
-
-function pieceDropped (e) {
-  document.onmousemove = null
-  document.onmouseup = null
-  if (currentDropPiece != null) {
-    const tmp = { xPos: currentPiece.xPos, yPos: currentPiece.yPos }
-    currentPiece.xPos = currentDropPiece.xPos
-    currentPiece.yPos = currentDropPiece.yPos
-    currentDropPiece.xPos = tmp.xPos
-    currentDropPiece.yPos = tmp.yPos
-  }
-  checkAnswer()
-}
-
-function checkAnswer () {
-  context.clearRect(0, 0, puzzleWidth, puzzleHeight)
-  let gameWin = true
-  let piece
-  for (let i = 0; i < pieces.length; i++) {
-    piece = pieces[i]
-    context.drawImage(img, piece.sx, piece.sy, pieceWidth, pieceHeight, piece.xPos, piece.yPos, pieceWidth, pieceHeight)
-    context.strokeRect(piece.xPos, piece.yPos, pieceWidth, pieceHeight)
-    if (piece.xPos != piece.sx || piece.yPos != piece.sy) {
-      gameWin = false
-    }
-  }
-  if (gameWin) {
-    document.onmousedown = null,
-    document.onmousemove = null,
-    document.onmouseup = null,
-    setTimeout(() =>
-      alert('victory'), 500)
-  }
 }
 
 window.onload = () => {
