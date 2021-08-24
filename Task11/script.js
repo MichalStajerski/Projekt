d26beb83de9e7811d570367395f44997f5637376
Michal Stajerski <m918560@gmail.com>
2021-08-24 15:08:07 +0200

    StandardJS cleanup

--- Task11/script.js
+++ Task11/script.js
@@ -1,19 +1,19 @@
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-setInterval(setTime, 1000);
+const minutesLabel = document.getElementById('minutes')
+const secondsLabel = document.getElementById('seconds')
+let totalSeconds = 0
+setInterval(setTime, 1000)// we refresh it every sedond because that is the minimum value by which timer is supposed to change
 
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+function setTime () {
+  ++totalSeconds
+  secondsLabel.innerHTML = pad(totalSeconds % 60)
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60))
 }
 
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {//if we have one number value or two number value
-    return "0" + valString;
+function pad (val) {
+  const valString = val + ''
+  if (valString.length < 2) { // if we have one number value or two number value
+    return '0' + valString
   } else {
-    return valString;
+    return valString
   }
 }
