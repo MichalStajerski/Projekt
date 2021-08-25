fd955eae70f1bcf5619934f9b546e2cd4c7aaa7d
Michal Stajerski <m918560@gmail.com>
2021-08-25 09:00:53 +0200

    changed timer

--- Task11/script.js
+++ Task11/script.js
@@ -1,19 +1,44 @@
-const minutesLabel = document.getElementById('minutes')
-const secondsLabel = document.getElementById('seconds')
-let totalSeconds = 0
-setInterval(setTime, 1000)// we refresh it every sedond because that is the minimum value by which timer is supposed to change
+// const minutesLabel = document.getElementById('minutes')
+// const secondsLabel = document.getElementById('seconds')
 
-function setTime () {
-  ++totalSeconds
-  secondsLabel.innerHTML = pad(totalSeconds % 60)
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60))
+
+function CreateLayout(startButton,body){
+  startButton.remove()
+  const counter = document.createElement('div')
+  counter.className = 'counter'
+  body.appendChild(counter)
+  const minutesLabel = document.createElement('label')
+  minutesLabel.id = 'minutes'
+  minutesLabel.innerHTML = '00'+':'
+  const secondsLabel = document.createElement('label')
+  secondsLabel.id = 'seconds'
+  secondsLabel.innerHTML = '00'
+  body.append(minutesLabel,secondsLabel)
+  let totalSeconds = 0
+  setInterval(setTime, 1000)// we refresh it every sedond because that is the minimum value by which timer is supposed to change
+
+  function setTime () {
+    ++totalSeconds
+    secondsLabel.innerHTML = pad(totalSeconds % 60)
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60))+':'
+  }
+
+  function pad (val) {
+    const valString = val + ''
+    if (valString.length < 2) { // if we have one number value or two number value
+      return '0' + valString
+    } else {
+      return valString
+    }
+  }
 }
 
-function pad (val) {
-  const valString = val + ''
-  if (valString.length < 2) { // if we have one number value or two number value
-    return '0' + valString
-  } else {
-    return valString
-  }
+window.onload = () =>{
+  const body = document.getElementById('body')
+  const startButton = document.createElement('button')
+  startButton.id = 'startButton'
+  startButton.className = 'glow-on-hover button-center'
+  startButton.innerHTML = 'Start'
+  startButton.onclick = () => CreateLayout(startButton,body)
+  body.appendChild(startButton)
 }
