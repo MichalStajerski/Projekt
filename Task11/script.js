7484b755d13fd06a31d4146d4503396c638e87df
Michal Stajerski <m918560@gmail.com>
2021-08-26 14:30:44 +0200

    cleanup

--- Task11/script.js
+++ Task11/script.js
@@ -7,10 +7,9 @@ canvas.style.border = '5px solid #000000'
 const ctx = canvas.getContext('2d')
 
 const hasSameColor = (color1, color2) => color1 === color2 // checks if two colors are the same
-const colorCombos = '0123456789ABCDEF'
-const colors= {
-  correctChoice : '#008000',
-  wrongChoice : '#ff0000'
+const colors = {
+  correctChoice: '#008000',
+  wrongChoice: '#ff0000'
 }
 let score = 0
 let fallTimer
@@ -26,25 +25,25 @@ var objects = {
     const removals = []
     for (let i = 0; i < objects.number.length; i++) {
       objects.centers[i][1] = objects.centers[i][1] + objects.stepSize[i]
-      if (objects.centers[i][0] > 600) {// if coordinate y surpasses 600 stop drawing circle
+      if (objects.centers[i][0] > 600) { // if coordinate y surpasses 600 stop drawing circle
         removals.push(i)
       }
     }
-    for (j = 0; j < removals.length; j++) {//delte all properties of a clicked object
+    for (j = 0; j < removals.length; j++) { // delte all properties of a clicked object
       objects.centers.splice(removals[j], 1)
       objects.sizes.splice(removals[j], 1)
       objects.color.splice(removals[j], 1)
       objects.stepSize.splice(removals[j], 1)
       objects.number[i] = objects.number[i] - 1
     }
-    objects.drawCircles()// after figuring out which circles not include into canvas proceed to drawing
+    objects.drawCircles()// after figuring out which circles not include into drawing
   },
   drawCircles: function () {
     for (i = 0; i < objects.number.length; i++) {
       ctx.beginPath()
       ctx.arc(objects.centers[i][0], objects.centers[i][1],
         objects.sizes[i], 0, 2 * Math.PI, false)
-      //ctx.fillStyle = objects.colors[i]
+      ctx.fillStyle = objects.colors[i]
       ctx.fill()
     }
   }
@@ -63,17 +62,17 @@ function CreateLayout (startButton, body) {
   counter.append(minutesLabel, secondsLabel)
   const scoreLabel = document.createElement('div')
   scoreLabel.id = 'scoreLabel'
-  scoreLabel.className = 'scoreLabel' 
-  body.append(scoreLabel,counter,canvas)
+  scoreLabel.className = 'scoreLabel'
+  body.append(scoreLabel, counter, canvas)
   let totalSeconds = 0
-  var time = setInterval(setTime, 1000)// we refresh it every sedond because that is the minimum value by which timer is supposed to change
+  const time = setInterval(setTime, 1000)// we refresh it every sedond because that is the minimum value by which timer is supposed to change
 
   function setTime () {
     ++totalSeconds
     secondsLabel.innerHTML = pad(totalSeconds % 60)// modulo for seconds
     minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60)) + ':'
-    console.log('minuteslabel',minutesLabel.innerHTML)
-    if(minutesLabel.innerHTML === '01:'){
+    console.log('minuteslabel', minutesLabel.innerHTML)
+    if (minutesLabel.innerHTML === '01:') {
       clearInterval(fallTimer)// clears interval for drawing falling objects to stop fall of the objects
       clearInterval(time)// clears interval for timer to stop counting
       alert('Game Over!')
@@ -88,7 +87,6 @@ function CreateLayout (startButton, body) {
     }
   }
 
-  
   fallTimer = setInterval(function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath()
@@ -99,15 +97,15 @@ function CreateLayout (startButton, body) {
     if (newCircle % objects.time == 0) {
       const xPos = getRandomIntInclusive(31, 569)// draws x position of canvas to drop the ball off
       objects.centers.push([xPos, 0])
-      objects.sizes.push(30)// decides the size of balls
-      let color = getRandomIntInclusive(0,1)
-      switch (color){
-        case 0: 
+      objects.sizes.push(30)// decides thr size of balls
+      let color = getRandomIntInclusive(0, 1)
+      switch (color) {
+        case 0:
           color = colors.correctChoice
-        break;
+          break
         case 1:
           color = colors.wrongChoice
-        break;
+          break
       }
       objects.colors.push(color)
       objects.stepSize.push(3)// stepsize decides the pace of falling down of objects
@@ -130,12 +128,11 @@ function rgbToHex (r, g, b) {
   return ((r << 16) | (g << 8) | b).toString(16)
 }
 
-
-function getMousePosition(canvas, event) {
-  let rect = canvas.getBoundingClientRect();
-  let x = event.clientX - rect.left;
-  let y = event.clientY - rect.top;
-  console.log("Coordinate x: " + x, "Coordinate y: " + y);
+function getMousePosition (canvas, event) {
+  const rect = canvas.getBoundingClientRect()
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+  console.log('Coordinate x: ' + x, 'Coordinate y: ' + y)
 }
 
 // canvas.addEventListener("mousedown", function(e)
@@ -146,21 +143,19 @@ function getMousePosition(canvas, event) {
 // });
 
 canvas.onclick = clickCirlce
-function clickCirlce(e){
+function clickCirlce (e) {
   console.log('passes')
   const pixel = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data // get the information about the pixel we clicked on
   const colorhex = '#' + ('000000' + rgbToHex(pixel[0], pixel[1], pixel[2])).slice(-6)// converts the data of pixel into hex notification of color
-  console.log('colorhex',colorhex)
-  ctx.clearRect(0,0, canvas.width, canvas.height)
+  console.log('colorhex', colorhex)
   const scoreLabel = document.getElementById('scoreLabel')
-  if(hasSameColor(colorhex,colors.correctChoice)){
+  if (hasSameColor(colorhex, colors.correctChoice)) {
     ++score
-    scoreLabel.innerHTML =score
-    console.log('score',score)
+    scoreLabel.innerHTML = score
+    console.log('score', score)
   }
-  getMousePosition(canvas,e)// gets the coordinates of the mouse in canvas upon click
+  getMousePosition(canvas, e)// gets the coordinates of the mouse in canvas upon click
 }
-
 
 window.onload = () => {
   const body = document.getElementById('body')
