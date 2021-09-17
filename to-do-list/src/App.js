d3d0aec792db72156e340fe5a1c5b735a668e32a
Michal Stajerski <m918560@gmail.com>
2021-09-17 13:45:36 +0200

    code cleanup

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -2,10 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react'
 import data from './data.json'
-import Header  from './header';
+import Header  from './Header';
 import ToDoList from './ToDoList'
-
-
 
 function App() {
   const [toDoList,setToDoList] = useState(data)
