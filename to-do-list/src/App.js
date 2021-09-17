8e2c20095ea4725e7970161b0bc6ad902b50e389
Michal Stajerski <m918560@gmail.com>
2021-09-17 14:07:48 +0200

    implemented adding new tasks

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import data from './data.json'
 import Header from './Header'
 import ToDoList from './ToDoList'
+import ToDoForm from './ToDoForm'
 
 function App () {
   const [toDoList, setToDoList] = useState(data)
@@ -21,11 +22,18 @@ function App () {
     setToDoList(filtered)
   }
 
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
   return (
     <div className='App'>
       <img src={logo} className='App-logo' alt='logo' />
       <Header />
       <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter} />
+      <ToDoForm addTask = {addTask}/>
     </div>
   )
 }
