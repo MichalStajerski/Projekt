0f7405536167822b281918e2ae6c278a1e42b7c6
Michal Stajerski <m918560@gmail.com>
2021-09-17 12:08:06 +0200

    code cleanup

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,12 +6,18 @@ import ToDoList from './ToDoList'
 
 
 function App() {
-  const [toDoList,setToDOList] = useState(data)
+  const [toDoList,setToDoList] = useState(data)
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
 
   return (
     <div className="App">
       <Header/>
-      <ToDoList toDoList ={toDoList}/>
+      <ToDoList toDoList ={toDoList} handleToggle = {handleToggle}/>
     </div>
   );
 }
