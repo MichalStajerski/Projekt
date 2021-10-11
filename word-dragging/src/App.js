60ae09d9b3fe8e2976998d5975b289002d6f62e7
Michal Stajerski <m918560@gmail.com>
2021-10-11 10:46:22 +0200

    blocked dragula drag and drop after correct answer was submitted

--- word-dragging/src/App.js
+++ word-dragging/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react'
 import Dragula from 'react-dragula';
+import dragula from 'dragula';
 
 const sentences = [
   'تكون أو لا تكون',
@@ -13,32 +14,32 @@ const sentences = [
 
 const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 const shuffleArray = (array) => array.sort(() => Math.random() - 0.5)
-const answersAreEqual = (answer,array) => array.includes(answer)
 
 const drawnAnswer = getRandomIntInclusive(0, sentences.length - 1)
 const words = sentences[drawnAnswer].trim().split(' ')
-const wordsOrder = []
-
-function drawOrderOfWords () {
-  for (let i = 0; i < words.length; i++) {
-    wordsOrder.push(i)
-  }
-  shuffleArray(wordsOrder)
-}
+const wordsOrder = shuffleArray([...Array(words.length).keys()]) // fills array with number from 0 to words.length - 1, then shuffles it
 
 function CheckAnswer(){
   const answer = document.getElementsByClassName('droptarget')
-  console.log('asnwer',answer)
   const answerToArray = Array.from(answer)
   const wordsSet = Array.from(answerToArray.map((word) => word.innerHTML))
   const finalSentence = answerToArray.map((word) => word.innerHTML).toString().replace(/,/g,' ')
-  console.log('answerString',finalSentence)
-  console.log('arrayFromWordSet',wordsSet)
   console.log(words)
   for (let i = 0; i < words.length; i++) {
     document.querySelector('#word'+i).style.backgroundColor = wordsSet[i] !== words[i] ? 'red' : 'silver'
   }
-  if(answersAreEqual(finalSentence,sentences)){
+
+  if(sentences[drawnAnswer] === finalSentence){
+    dragula([].slice.apply(document.querySelectorAll('.wordsContainer')),{
+     accepts: (el, target, source, sibling) => {
+       if (!target || !source || (target != source)) {
+         return false;
+       } else {
+         return true;
+       }
+     }
+   });
+
     setTimeout(()=>{
       alert('Vicotry')
     },200)
@@ -51,7 +52,6 @@ function CheckAnswer(){
 
 class Board extends React.Component{
   render(){
-    drawOrderOfWords()
     var numberOfWords = []
     for (let i = 0; i < words.length; i++) {
       numberOfWords.push(
@@ -72,6 +72,7 @@ class Board extends React.Component{
       </div>
     )
   }
+  
   dragulaDecorator = (componentBackingInstance) => {
     if (componentBackingInstance) {
       let options = { };
@@ -80,13 +81,14 @@ class Board extends React.Component{
   }
 }
 
-class Game extends React.Component {  
+class Game extends React.Component {
+  cons
   render(){
     return(
       <div>
         <Board/>
         <div class = 'row'>
-          <button id ='btnCheck' className = 'glow-on-hover button-center' onClick = {CheckAnswer} >Submit</button>
+          <button id ='btnCheck' className = 'glow-on-hover button-center'  onClick = {CheckAnswer} >Submit</button>
           <button className = 'glow-on-hover button-center' onClick= {() => window.location.reload()}>Reload</button>
         </div>
       </div> 
