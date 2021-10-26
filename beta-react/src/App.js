6bf7e8617744a55071a77374c39d94841ef889b4
Michal Stajerski <m918560@gmail.com>
2021-10-26 10:31:09 +0200

    package update and buttons added

--- beta-react/src/App.js
+++ beta-react/src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Profile from './Profile';
-import { people } from './data';
+import logo from './logo.svg'
+import './App.css'
+import Profile from './Profile'
+import { people } from './data'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-export default function Gallery () {
-    return(
-        <section>
+export default function App () {
+  function Gallery () {
+    return (
+      <section>
         {people.map(person => (
-            <Profile 
-                key = {person.id}
-                name = {person.name}
-                imageId = {person.imageId}
-            />
+          <Profile
+            key={person.id}
+            name={person.name}
+            imageId={person.imageId}
+          />
         ))}
-        </section>
-    );
+      </section>
+    )
+  }
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <button>
+            <Link to='/'>Gallery</Link>
+          </button>
+          <br />
+          <button>
+            <Link to='/about'>About</Link>
+          </button>
+        </nav>
+        <Switch>
+          <Route path='/about' />
+          <Route path='/'>
+            <Gallery />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
--- beta-react/src/Profile.js
+++ beta-react/src/Profile.js
@@ -1,14 +1,13 @@
-export default function Profile ({name, imageId}) {
-    const imageUrl = (`https://i.imgur.com/${imageId}s.jpg`)
-    return (
-        <div>
-        <h1>{name}</h1>
-        <img
-            className = 'avatar'
-            alt = {name}
-            src = {imageUrl}
-        />
-        </div>
-    );
+export default function Profile ({ name, imageId }) {
+  const imageUrl = (`https://i.imgur.com/${imageId}s.jpg`)
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img
+        className='avatar'
+        alt={name}
+        src={imageUrl}
+      />
+    </div>
+  )
 }
-  
