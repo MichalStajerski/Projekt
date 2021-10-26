512eda62fdfa8d5c20c6153367503680b8123a3e
Michal Stajerski <m918560@gmail.com>
2021-10-26 13:48:28 +0200

    added products category component

--- beta-react/src/Products.js
+++ beta-react/src/Products.js
@@ -1,38 +1,47 @@
 import { useState } from 'react'
-
-function ProductsCategory ({ category }) {
-  return (
-    <tr>
-      <th>
-        {category}
-      </th>
-    </tr>
-  )
-}
-
-function ProductRow ({ product }) {
-  const name = product.inStock ? product.name
-    : <span style={color = 'red'}>{product.name}</span>
-  return (
-    <tr>
-      <td>
-        {name}
-      </td>
-      <td>
-        {product.price}
-      </td>
-    </tr>
-  )
-}
-
-function SearchBar () {
+function ProductTable () {
   const [state, setState] = useState(false)
   const [search, setSearch] = useState('')
 
+  function ProductsCategory ({ category }) {
+    return (
+      <tr>
+        <th>
+          {category}
+        </th>
+      </tr>
+    )
+  }
+
+  function ProductRow ({ product }) {
+    const name = product.inStock ? product.name
+      : <span style={color = 'red'}>{product.name}</span>
+    return (
+      <tr>
+        <td>
+          {name}
+        </td>
+        <td>
+          {product.price}
+        </td>
+      </tr>
+    )
+  }
+
+  function SearchBar () {
+    return (
+      <section>
+        <input onChange={e => setSearch(e.target.value)}>Search</input>
+        <input type='checkbox' onChange={setState(!state)} />
+      </section>
+    )
+  }
+
   return (
-    <section>
-      <input onChange = {e => setSearch(e.target.value)}>Search</input>
-      <input type = 'checkbox' onChange = {setState(!state)}></input>
-    </section>
+    <state>
+      <SearchBar />
+      <ProductsCategory />
+      <ProductRow />
+    </state>
   )
 }
