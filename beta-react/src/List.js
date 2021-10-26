18272e2e99208da2f749fb7fbad3463db7b2cd1c
Michal Stajerski <m918560@gmail.com>
2021-10-26 12:14:59 +0200

    code cleanup

--- beta-react/src/List.js
+++ beta-react/src/List.js
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react'
 let nextId = 0
 
 export default function List () {
-    const [task, setTask] = useState('')
-    const [list, setList] = useState([])
+  const [task, setTask] = useState('')
+  const [list, setList] = useState([])
 
-    return (
-      <>
-        <h1>List:</h1>
-        <input
-          value={task}
-          onChange={e => setTask(e.target.value)}
-        />
-        <button onClick={() => {
-          setList([
-            ...list,
-            { id: nextId++, task: task }
-          ])
-        }}
-        >Add
-        </button>
-        <ul>
-          {list.map(list => (
-            <li key={list.id}>{list.task}</li>
-          ))}
-        </ul>
-      </>
-    )
-  }
+  return (
+    <>
+      <h1>List:</h1>
+      <input
+        value={task}
+        onChange={e => setTask(e.target.value)}
+      />
+      <button onClick={() => {
+        setList([
+          ...list,
+          { id: nextId++, task: task }
+        ])
+      }}
+      >Add
+      </button>
+      <ul>
+        {list.map(list => (
+          <li key={list.id}>{list.task}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
