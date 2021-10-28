489ee3b5fd483e46fbd663d108ffbb452ea3f308
Michal Stajerski <m918560@gmail.com>
2021-10-28 09:01:45 +0200

    updated functions

--- beta-react/src/Products.js
+++ beta-react/src/Products.js
@@ -1,47 +1,53 @@
 import { useState } from 'react'
-function ProductTable () {
+function FillableProductTable () {
   const [state, setState] = useState(false)
   const [search, setSearch] = useState('')
 
-  function ProductsCategory ({ category }) {
-    return (
-      <tr>
-        <th>
-          {category}
-        </th>
-      </tr>
-    )
-  }
-
-  function ProductRow ({ product }) {
-    const name = product.inStock ? product.name
-      : <span style={color = 'red'}>{product.name}</span>
-    return (
-      <tr>
-        <td>
-          {name}
-        </td>
-        <td>
-          {product.price}
-        </td>
-      </tr>
-    )
-  }
-
-  function SearchBar () {
-    return (
-      <section>
-        <input onChange={e => setSearch(e.target.value)}>Search</input>
-        <input type='checkbox' onChange={setState(!state)} />
-      </section>
-    )
-  }
-
   return (
     <state>
-      <SearchBar />
+      <SearchBar 
+      state = {state}
+      search = {search}/>
       <ProductsCategory />
       <ProductRow />
     </state>
   )
+}
+function ProductsCategory ({ category }) {
+  return (
+    <tr>
+      <th>
+        {category}
+      </th>
+    </tr>
+  )
+}
+
+function ProductRow ({ product }) {
+  const name = product.inStock ? product.name
+    : <span style={color = 'red'}>{product.name}</span>
+  return (
+    <tr>
+      <td>
+        {name}
+      </td>
+      <td>
+        {product.price}
+      </td>
+    </tr>
+  )
+}
+
+function SearchBar ({state, search}) {
+  return (
+    <section>
+      <input onChange={e => setSearch(e.target.value)}>Search</input>
+      <input type='checkbox' onChange={setState(!state)} />
+    </section>
+  )
+}
+
+function ProductTable ({products,state,search}) {
+  const rows = []
+  
 }
