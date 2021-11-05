97115165835e05acaa2c2f43a406e698861ed6dc
MichalStajerski <m918560@gmail.com>
2021-11-05 10:32:47 +0100

    added 2.10.3

---
+++ 2.10.3/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component, useState } from 'react'
+import './App.css'
+import { toppings } from './toppings'
+const n = 2;
+
+class Example extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      // checked/unchecked is stored here
+      // initially the first one is checked:
+      // [true, false, false]
+      checkboxes: new Array(n).fill(false),
+    };
+  }
+  onChange(e, changedIndex) {
+    // it is a good habit to extract things from event variable
+    const { checked } = e.target;
+    
+    this.setState(state => ({
+      // this lets you unselect all.
+      // but selected can be anly one at a time
+      checkboxes: state.checkboxes.map((_, i) => i === changedIndex ? checked : false),
+    }));
+    setAnswer( {value : checked})
+  }
+  render() {
+    const { checkboxes } = this.state;
+    
+    return (
+      <div>
+        {checkboxes.map((item, i) => (
+          <input
+            key={i}
+            type="checkbox"
+            checked={item}
+            onChange={e => this.onChange(e, i) /* notice passing an index. we will use it */}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default function App () {
+const [answer, setAnswer] = useState([])
+
+ return(
+   <section>
+     <Example
+     setAnswer = {setAnswer}/>
+     {console.log(answer)}
+   </section>
+ )
+}
---
+++ 2.10.3/src/toppings.js
@@ -0,0 +1,30 @@
+export const toppings = [
+  {
+    name: 'Capsicum',
+    value: true
+  },
+  {
+    name: 'Paneer',
+    value: true
+  },
+  {
+    name: 'Red Paprika',
+    value: false
+  },
+  {
+    name: 'Onions',
+    value: false
+  },
+  {
+    name: 'Extra Cheese',
+    value: true
+  },
+  {
+    name: 'Baby Corns',
+    value: true
+  },
+  {
+    name: 'Mushroom',
+    value: true
+  }
+]
